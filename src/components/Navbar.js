71bf2095dcc4253ed7138164bc9a9159b82f7177
Deepak <dpkgusain45@gmail.com>
2024-07-25 10:47:19 +0530

    cantilever_newsApp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,175 @@
-import React, { useContext, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { SearchContext } from '../context/SearchContext';
-import logo from './images/logo.svg'
+import React, { useContext, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { SearchContext } from "../context/SearchContext";
+import logo from "./images/logo.svg";
+// import { Avatar, Button, Menu, MenuItem } from "@mui/material";
+// import { useDispatch } from "react-redux";
+
 const Navbar = ({ title }) => {
-  const {search,setSearch} = useContext(SearchContext);
-  const location = useLocation()
-  useEffect(()=> {
-    console.log(location.pathname)
-  },[location])
+  const { search, setSearch } = useContext(SearchContext);
+  const location = useLocation();
+  // const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
+
+  // const handleUserClick = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+  // const handleCloseUserMenu = (event) => {
+  //   setAnchorEl(null);
+  // };
+
+  // const handleOpen = () => {
+  //   setOpenAuthModal(true);
+  // };
+  // const handleClose = () => {
+  //   setOpenAuthModal(false);
+  // };
+
+  // const handleLogout = () =>{
+  //   dispatch(logout())
+  //   handleCloseUserMenu()
+  // }
+  
+
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg" style={{backgroundColor: '#5A9', color: '#FFF'}}>
+      <nav
+        className="navbar navbar-expand-lg"
+        style={{ backgroundColor: "#5A9", color: "#FFF" }}
+      >
         <div className="container-fluid">
-          <img src={logo} alt='logo' width={50}></img>
-          <Link className="navbar-brand" to="/" style={{fontFamily: 'Arial, sans-serif', fontSize: '1.5em'}}>{title}</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <img src={logo} alt="logo" width={50}></img>
+          <Link
+            className="navbar-brand"
+            to="/"
+            style={{ fontFamily: "Arial, sans-serif", fontSize: "1.5em" }}
+          >
+            {title}
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <div className="nav-item">
-                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/" style={{color: location.pathname==='/'? '#FFF':"lightgrey"}}>General</Link></li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/"
+                    style={{
+                      color: location.pathname === "/" ? "#FFF" : "lightgrey",
+                    }}
+                  >
+                    General
+                  </Link>
+                </li>
               </div>
-              <li className="nav-item"><Link className="nav-link" to="/business" style={{color: location.pathname==='/business'? '#FFF':"lightgrey"}}>Business</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/entertainment" style={{color: location.pathname==='/entertainment'? '#FFF':"lightgrey"}}>Entertainment</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/health" style={{color: location.pathname==='/health'? '#FFF':"lightgrey"}}>Health</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/science" style={{color: location.pathname==='/science'? '#FFF':"lightgrey"}}>Science</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/sports" style={{color: location.pathname==='/sports'? '#FFF':"lightgrey"}}>Sports</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/technology" style={{color: location.pathname==='/technology'? '#FFF':"lightgrey"}}>Technology</Link></li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/business"
+                  style={{
+                    color:
+                      location.pathname === "/business" ? "#FFF" : "lightgrey",
+                  }}
+                >
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/entertainment"
+                  style={{
+                    color:
+                      location.pathname === "/entertainment"
+                        ? "#FFF"
+                        : "lightgrey",
+                  }}
+                >
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/health"
+                  style={{
+                    color:
+                      location.pathname === "/health" ? "#FFF" : "lightgrey",
+                  }}
+                >
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/science"
+                  style={{
+                    color:
+                      location.pathname === "/science" ? "#FFF" : "lightgrey",
+                  }}
+                >
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/sports"
+                  style={{
+                    color:
+                      location.pathname === "/sports" ? "#FFF" : "lightgrey",
+                  }}
+                >
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/technology"
+                  style={{
+                    color:
+                      location.pathname === "/technology"
+                        ? "#FFF"
+                        : "lightgrey",
+                  }}
+                >
+                  Technology
+                </Link>
+              </li>
             </ul>
+
+            
+
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" value={search} aria-label="Search" onChange={(ev)=>setSearch(ev.target.value)} style={{marginRight: '10px'}}/>
-              <button className="btn btn-outline-light" type="submit">Search</button>
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                value={search}
+                aria-label="Search"
+                onChange={(ev) => setSearch(ev.target.value)}
+                style={{ marginRight: "10px" }}
+              />
+              <button className="btn btn-outline-light" type="submit">
+                Search
+              </button>
             </form>
           </div>
         </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Spinner from './Spinner';
-import NewsItem from './NewsItem';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect, useContext } from "react";
+import Spinner from "./Spinner";
+import NewsItem from "./NewsItem";
+import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { SearchContext } from '../context/SearchContext';
-import noNews from './images/notFound.svg'
+import { SearchContext } from "../context/SearchContext";
+import noNews from "./images/notFound.svg";
 const News = (props) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
   const [totalResults, setTotalResults] = useState(0);
-    const {search} = useContext(SearchContext)
-    useEffect(()=> {
-        console.log(search)
-    },[search])
+  const { search } = useContext(SearchContext);
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
   const updateNews = async () => {
     props.setProgress(10);
     let url = `https://newsapi.org/v2/top-headlines?q=${search}&category=${props.category}&country=${props.country}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
@@ -22,7 +22,7 @@ const News = (props) => {
     props.setProgress(30);
     let parsedData = await data.json();
     let articles = parsedData.articles;
-    articles.length===0 && setLoading(false);
+    articles.length === 0 && setLoading(false);
     props.setProgress(50);
     setArticles(articles);
     setLoading(false);
@@ -30,10 +30,10 @@ const News = (props) => {
     props.setProgress(100);
     setPage(page + 1);
   };
-  useEffect(()=> {
+  useEffect(() => {
     updateNews();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[search])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]);
   useEffect(() => {
     document.title = `${props.category} - News-App`;
     updateNews();
@@ -53,41 +53,64 @@ const News = (props) => {
   };
 
   return (
-    <div >
-      <h2 className='text-center my-5'>News Now- Top Headlines</h2>
+    <div>
+      <h2 className="text-center my-5">News Now- Top Headlines</h2>
       {loading && <Spinner />}
       <div className="container">
-      {
-        articles.length!==0? <InfiniteScroll
-        dataLength={articles.length}
-        next={fetchMoreData}
-        hasMore={articles.length < totalResults}
-        loader={<Spinner/>}
-      >
-        <div className="container-fluid">
-          <div className="row my-3">
-            {articles.map((el,index) => (
-              <div className="col-sm-12 col-md-6 col-lg-4" key={index}>
-                <NewsItem title={el.title ? el.title.slice(0, 44) : ""} description={el.description ? el.description.slice(0, 88) : ""} img={el.urlToImage} imgurl={el.url} author={el.author ? el.author : "Unknown"} date={el.publishedAt} sourceName={el.source.name} />
+        {articles.length !== 0 ? (
+          <InfiniteScroll
+            dataLength={articles.length}
+            next={fetchMoreData}
+            hasMore={articles.length < totalResults}
+            loader={<Spinner />}
+          >
+            <div className="container-fluid">
+              <div className="row my-3">
+                {articles.map((el, index) => (
+                  <div className="col-sm-12 col-md-6 col-lg-4" key={index}>
+                    <NewsItem
+                      title={el.title ? el.title.slice(0, 44) : ""}
+                      description={
+                        el.description ? el.description.slice(0, 88) : ""
+                      }
+                      img={el.urlToImage}
+                      imgurl={el.url}
+                      author={el.author ? el.author : "Unknown"}
+                      date={el.publishedAt}
+                      sourceName={el.source.name}
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <div
+            style={{
+              width: "50%",
+              height: "50%",
+              margin: "auto",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src={noNews}
+              alt="no news found"
+              style={{ width: "inherit" }}
+            ></img>
           </div>
-        </div>
-      </InfiniteScroll> : 
-        <div style={{width:"50%",height:"50%",margin:"auto",display:"flex",justifyContent:"center",alignItems:"center"}}>
-
-      <img src={noNews} alt='no news found' style={{width:"inherit"}}></img>
-      </div>
-      }  
+        )}
       </div>
     </div>
   );
 };
 
 News.defaultProps = {
-  country: 'in',
+  country: "in",
   pageSize: 5,
-  category: 'general'
+  category: "general",
 };
 
 News.propTypes = {
